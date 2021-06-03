894f2ea2fc3ef2b5fb2a3f1df4f9bc81d40f0d48
xiaocq <516563076@qq.com>
2021-06-03 23:00:47 +0800

    1

--- app/src/main/java/com/example/testfmaarsecond/MainActivity.java
+++ app/src/main/java/com/example/testfmaarsecond/MainActivity.java
@@ -1,13 +1,52 @@
 package com.example.testfmaarsecond;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.facilityone.wireless.patrol.PatrolActivity;
+
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView tv1;
+    private Button btn2;
+    private Button btn3;
+    private Button btn4;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        initView();
+        initListeners();
+    }
+
+    private void initListeners() {
+        btn2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, PatrolActivity.class);
+                MainActivity.this.startActivity(intent);
+            }
+        });
+        btn3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+//                Intent intent = new Intent(MainActivity.this, TestYokeyActivity.class);
+//                MainActivity.this.startActivity(intent);
+            }
+        });
+    }
+
+    private void initView() {
+        tv1 = (TextView) findViewById(R.id.tv1);
+        btn2 = (Button) findViewById(R.id.btn2);
+        btn3 = (Button) findViewById(R.id.btn3);
+        btn4 = (Button) findViewById(R.id.btn4);
     }
 }
